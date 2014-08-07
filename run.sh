124425cb1c57d948ee0c122744ca896b4c65cace
David Capwell <dcapwell@David-Capwell-MacBook-Pro.local>
2014-08-06 17:18:10 -0700

    run script

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+build() {
+  mvn clean install && mvn assembly:single
+}
+
+run() {
+  ./bin/ycsb load tachyon -P workloads/workloadd -p address=tachyon://master:19998
+}
+
+main() {
+  local cmd="${1:-run}"
+  $cmd
+}
+
+main "$@"
+
