81f428f7bc9189566046eff5dad48627b3454c5e
David Capwell <dcapwell@David-Capwell-MacBook-Pro.local>
2014-10-28 15:25:17 -0700

    added run and env script

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+bin=`dirname "$0"`
+bin=`cd "$bin">/dev/null; pwd`
+
+. $bin/ycsb-env.sh
+
+build() {
+  mvn clean install && mvn assembly:single
+}
+
+clean() {
+  pushd $TACHYON_HOME
+  ./bin/tachyon tfs rm usertable
+  popd
+}
+
+load() {
+  ./bin/ycsb load tachyon $YCSB_OPS 1> load.log 2> load.err
+}
+
+run() {
+  ./bin/ycsb run tachyon $YCSB_OPS 1> run.log 2> run.err
+}
+
+main() {
+  local cmd="${1:-run}"
+  $cmd
+}
+
+main "$@"
+
---
+++ ycsb-env.sh
@@ -0,0 +1,21 @@
+export WORKLOAD=${WORKLOAD:-read-stress}
+case "`uname`" in
+  Linux|FreeBSD|SunOS)
+    export NUM_THREADS=${NUM_THREADS:-$(nproc)}
+  ;;
+  Darwin)
+    export NUM_THREADS=${NUM_THREADS:-$(sysctl hw.ncpu | awk '{print $2}')}
+  ;;
+  *)
+    export NUM_THREADS=${NUM_THREADS:-2}
+  ;;
+esac
+
+export FIELD_COUNT=${FIELD_COUNT:-4}
+export FIELD_LENGTH=${FIELD_LENGTH:-100}
+export TACHYON_HOME=${TACHYON_HOME:-/opt/tachyon/tachyon-0.6.0-SNAPSHOT}
+
+export MASTER_HOSTNAME=${MASTER_HOSTNAME:-$(hostname -f)}
+export MASTER_ADDRESS=${MASTER_ADDRESS:-"tachyon://$MASTER_HOSTNAME:19998"}
+
+export YCSB_OPS=${YCSB_OPS:-"-P workloads/${WORKLOAD} -p fieldcount=${FIELD_COUNT} -p fieldlength=${FIELD_LENGTH} -p uri=$ MASTER_ADDRESS -threads $NUM_THREADS -s"}
